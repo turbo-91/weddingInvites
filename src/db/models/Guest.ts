bb0911e63ec7dcb2615d883f095279b0840599c8
Torben Jost <torbenjost@proton.me>
2025-04-15 17:32:23 +0200

    setup MongoDB with Guest model

---
+++ src/db/models/Guest.ts
@@ -0,0 +1,35 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export type RSVPStatus = "yes" | "no" | "pending";
+
+export interface IGuest extends Document {
+  _id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  inviteId: string;
+  rsvp: RSVPStatus;
+  dietRestrictions: string;
+  plusOneAllowed: boolean;
+  plusOneName: string;
+}
+
+const guestSchema = new Schema<IGuest>({
+  _id: { type: Number, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  inviteId: { type: String, required: true },
+  rsvp: {
+    type: String,
+    enum: ["yes", "no", "pending"],
+    default: "pending",
+  },
+  dietRestrictions: { type: String, required: true },
+  plusOneAllowed: { type: Boolean, required: true },
+  plusOneName: { type: String, required: true },
+});
+
+const Guest = mongoose.models.Guest || mongoose.model("Guest", guestSchema);
+
+export default Guest;
